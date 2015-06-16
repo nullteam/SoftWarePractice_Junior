87e4ed159051125350556807f6cdfd3fe9551eb8
张满 <1304523137@qq.com>
2015-06-16 16:17:14 +0800

    更新LoginActivity,新添了一个util包中的类

--- SlidingMenu-master/src/com/universer/HustWhereToEat/activity/LoginActivity.java
+++ SlidingMenu-master/src/com/universer/HustWhereToEat/activity/LoginActivity.java
@@ -1,10 +1,17 @@
 package com.universer.HustWhereToEat.activity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.provider.SyncStateContract.Constants;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +20,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.universer.HustWhereToEat.R;
+import com.universer.HustWhereToEat.http.HWAsyncHttpClient;
+import com.universer.HustWhereToEat.http.HWResponseHandler;
+import com.universer.HustWhereToEat.util.SharedPreferencesUtil;
 
 public class LoginActivity extends Activity {
 	private Button login_Button;
@@ -20,17 +30,40 @@ public class LoginActivity extends Activity {
 	private Button forwardseeBtn;
 	private EditText user;
 	private EditText password;
+	
+	private String factorOneStr;
+	private String factorTwoStr;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 
+		init();
+		bindEvent();
+		
+	}
+	
+	private void init() {
 		user = (EditText) findViewById(R.id.user);
 		password = (EditText) findViewById(R.id.password);
 		login_Button = (Button) findViewById(R.id.login);
 		registButton = (Button) findViewById(R.id.regist_button);
 		forwardseeBtn = (Button) findViewById(R.id.forwardsee);
+	}
+	
+	@SuppressLint("NewApi")
+	private void autoLogin() {
+		SharedPreferences curreentAccountsPrefs = SharedPreferencesUtil.
+									setSettingSharedPreferences(getApplicationContext());
+		factorOneStr = curreentAccountsPrefs.getString("account","");
+		factorTwoStr = curreentAccountsPrefs.getString("password","");
+		if(!factorOneStr.isEmpty() && factorTwoStr.isEmpty()){
+			requestLogin();
+		}
+	}
+	
+	private void bindEvent() {
 		registButton.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -48,24 +81,59 @@ public class LoginActivity extends Activity {
 		});
 		login_Button.setOnClickListener(new OnClickListener() {
 
+			@SuppressLint("NewApi")
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				String factorOneStr = user.getText().toString();
-				String factorTwoStr = password.getText().toString();
-				System.out.println("cat");
-				if ("universer".equals(factorOneStr)
-						&& "123456".equals(factorTwoStr)) {
-
-					Intent intent = new Intent();
-					intent.putExtra("one", factorOneStr);
-					intent.putExtra("two", factorTwoStr);
-					intent.setClass(LoginActivity.this, SlidingActivity.class);
-					LoginActivity.this.startActivity(intent);
+				factorOneStr = user.getText().toString();
+				factorTwoStr = password.getText().toString();
+				if(factorOneStr.isEmpty()){
+					Toast.makeText(LoginActivity.this,"请输入用户名", Toast.LENGTH_SHORT).show();
+				}else if(factorTwoStr.isEmpty()){
+					Toast.makeText(LoginActivity.this,"请输入密码", Toast.LENGTH_SHORT).show();;
+				}else{
+					requestLogin();
 				}
+				System.out.println("cat");
+				
 			}
 		});
+	}
+	
+	private void requestLogin(){
+		HWAsyncHttpClient client = new HWAsyncHttpClient();
+		client.post(LoginActivity.this, "", null, new HWResponseHandler(){
 
+			@Override
+			public void onSuccess(JSONObject jo) {
+				try {
+					String result = jo.getString("result");
+					if(result.equals("")){
+						
+					}else{
+						SharedPreferences accountsPrefs = SharedPreferencesUtil.
+								setUserSharedPreference(LoginActivity.this, factorOneStr);
+						
+						Editor editor=accountsPrefs.edit();
+						editor.putString("passWord", factorTwoStr);
+						Intent intent = new Intent();
+						intent.putExtra("one", factorOneStr);
+						intent.putExtra("two", factorTwoStr);
+						intent.setClass(LoginActivity.this, SlidingActivity.class);
+						LoginActivity.this.startActivity(intent);
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				super.onSuccess(jo);
+			}
+
+			@Override
+			public void onFailure() {
+				// TODO Auto-generated method stub
+				super.onFailure();
+			}
+			
+		});
 	}
 
 	@Override
---
+++ SlidingMenu-master/src/com/universer/HustWhereToEat/util/SharedPreferencesUtil.java
@@ -0,0 +1,37 @@
+package com.universer.HustWhereToEat.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesUtil {
+	public static final String USER_ID="user_name";
+	public static final String SETTING="setting";
+	public static final String CURRENT_ACCOUNT="currenrAccount";
+	
+	public static SharedPreferences setUserSharedPreference(Context context,String userName){
+		SharedPreferences userSharedPreferences=context.getSharedPreferences(userName,Context.MODE_PRIVATE);
+		
+		return userSharedPreferences;
+	}
+	
+	public static int getUserIntShare(Context context,String key,int defaultValue,String userName){
+		SharedPreferences userSharedPreferences=context.getSharedPreferences(userName,Context.MODE_PRIVATE);
+		return userSharedPreferences.getInt(key, defaultValue);
+	}
+	
+	public static String getUserStringShare(Context context,String key,String defaultValue){
+		String useId="";
+		SharedPreferences userSharedPreferences=context.getSharedPreferences(USER_ID+useId,Context.MODE_PRIVATE);
+		return userSharedPreferences.getString(key, defaultValue);
+	}
+	
+	public static SharedPreferences setSettingSharedPreferences(Context context){
+		SharedPreferences settingSharedPreferences=context.getSharedPreferences(SETTING,Context.MODE_PRIVATE);
+		return settingSharedPreferences;
+	}
+	
+	public static SharedPreferences setCurrentAccountSharedPreferences(Context context){
+		SharedPreferences setCurrentAccountSharedPreferences=context.getSharedPreferences(CURRENT_ACCOUNT,Context.MODE_PRIVATE);
+		return setCurrentAccountSharedPreferences;
+	}
+}
