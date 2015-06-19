f45f31d92dabcef1e327b438c45bad1b4dcd0cd9
nullteam <13098840196@163.com>
2015-06-19 21:29:37 +0800

    SAE

--- SlidingMenu-master/src/com/universer/HustWhereToEat/util/Constant.java
+++ SlidingMenu-master/src/com/universer/HustWhereToEat/util/Constant.java
@@ -1,5 +1,5 @@
 package com.universer.HustWhereToEat.util;
 
 public class Constant {
-	public static final String BASE_URL = "http://feifeiiong.eicp.net/WalkingDelicious";
+	public static final String BASE_URL = "http://1.walkingdelicious.sinaapp.com/";
 }
